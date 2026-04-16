1c12cc5cdd14350d1012d0f06aaad58d1fec9c4a
mgangula3-gif <mgangula3@gitam.in>
2026-04-16 11:50:35 +0530

    Create DBconnection.java

---
+++ DBconnection.java
@@ -0,0 +1,23 @@
+package com.db;
+
+import java.sql.*;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/jspdb",
+                "root",
+                "password"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
